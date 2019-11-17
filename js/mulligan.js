8084b46e2ddf2bf747da8722621dd87e1563dea8
Querijn Heijmans <querijn@irule.at>
2019-11-17 19:04:16 +0100

    Flip cards for reveal

--- js/mulligan.js
+++ js/mulligan.js
@@ -13,15 +13,18 @@ export default class Mulligan {
 
 	addCards(cards) {
 
-		const width = 400; // TODO: Make variable
+		const width = 300; // TODO: Make variable
 		if (!cards || cards.length == 0)
 			return;
 
-		const left = this.x - width / 2 + cards[0].width / 2;
+		const left = this.x - width / 2.733; // TODO: This is now completely wrong but whatever.
 		for (let i = 0; i < 4; i++) {
 			const card = cards[i];
 			this.cards.push(card);	
-			card.moveTo(left + (width / 4) * i, this.y, 250 * (i + 1));
+			const anim = card.moveTo(left + (width / 4) * i, this.y, 250 * (i + 1)) // Move to mulligan position.
+			
+			if (!this.player.isTop) // Flip if bottom player
+				anim.onDone((anim) => { card.flip() });
 		}
 	}
 
