ea5c7210ce7d130435d010b603885f93591cab04
Querijn Heijmans <querijn@irule.at>
2019-11-20 19:39:13 +0100

    Revert mulligan position code

--- js/mulligan.js
+++ js/mulligan.js
@@ -31,7 +31,7 @@ export default class Mulligan {
 		};
 
 		let anim = null;
-		const left = this.x - width / 2;
+		const left = this.x - width / 2.733; // TODO: This is now completely wrong but whatever.
 		for (let i = 0; i < cards.length; i++) {
 
 			const index = pos != null ? pos : i;
@@ -40,7 +40,7 @@ export default class Mulligan {
 			this.cards.push(card);
 			const moveDuration = skipAnimations ? 0 : 250 * (index + 1);
 			anim = card.moveTo( // Move to mulligan position.
-				left + (width / (this.cards.length + 1)) * (index + 1),		// x
+				left + (width / 4) * index,		// x
 				this.y, 													// y
 				3, 															// z
 				moveDuration												// move duration
